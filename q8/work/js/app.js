421221611daef92b6df3261466bc5ba4b7743ddc
kzhyk <halcutlery.s@gmail.com>
2022-10-20 21:54:10 +0900

    check commit

--- q8/work/js/app.js
+++ q8/work/js/app.js
@@ -1,33 +1,55 @@
-// API
-    const settings = {
-    "url": 'https://ci.nii.ac.jp/books/opensearch/search?title=${searchWord}&format=json&p=${pageCount}&count=20',
-    "method": "GET",
+
+    function displayResult(response){
+        $('.inner').html(response);
     }
-    $.ajax(settings).done(function (response) {
-        const result = response['@graph'];
-        displayResult(result)
-        }).fail(function (err) {
-        displayError(err)
-        });
+    function displayError(err) {
+        $('.inner').html(err);
+        err = '正常に通信できませんでした。</br>インターネットの接続の確認をしてください。';
+    }
+
+    let preSearchWord = null;
+    let searchWord = null;
+    let pageCount = 1    
+
 // 検索
-    $(".search-btn").on("click", function() {
-    // 一回目の入力処理
-        var text = $("#search-input").val();
-        searchWord = text;
-        pageCount = 1;
-    });
-    // 二回目以降の処理
-    $(".search-btn").on("click", function() {
-        var text2 = $("#search-input").val();
-        searchWord = text2;
-        if (text == text2) {
-            pageCount = pageCount++;
+
+    // 二回目以降一致していた場合、しない場合
+    $('.search-btn').on('click', function() {
+        if(preSearchWord !== null && preSearchWord === searchWord) {
+            pageCount++;
         } else {
-            pegeCount = 1;
+            pageCount = 1;
         }
+        
+        preSearchWord = searchWord
+        
+        console.log(searchWord);
+        console.log(pageCount);    
+
+        // エラー処理
+        if (searchWord = null) {
+            $(".inner").append('<div class="message">検索キーワードが有効ではありません。</br>1文字以上で検索して下さい。</div>');
+        };
+
+        // リセットされた際の処理
+        $('.reset-btn').on('click', function() {
+            $('#search-input').val('');
+            searchWord = '';
+            pageCount = '';
+        }); 
+
+    // API
+    const settings = {
+        "url": `https://ci.nii.ac.jp/books/opensearch/search?title=${searchWord}&format=json&p=${pageCount}&count=20`,
+        "method": "GET",
+        }
+        $.ajax(settings).done(function (response) {
+            const result = response['@graph'];
+            console.log(result)
+            displayResult(result)
+            })
+            .fail(function (err) {
+            displayError(err)
+            });    
     });
-
-
-    getParameter();
-        params['searchWord'] = text;
-        params['pageCount'] = 1;
+    // <div class="message">"検索結果が見つかりませんでした。"</br>"別のキーワードで検索して下さい。"</div>
